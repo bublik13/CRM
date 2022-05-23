baee59b8418c2b0ba3b5f1336b2b72a6a74831ef
Your Name <youremail@domain.com>
2022-05-23 18:32:52 +0300

    создается js для menu

--- app/js/main.js
+++ app/js/main.js
@@ -22,3 +22,24 @@ const swiper = new Swiper('.swiper1', {
     spaceBetween: 10
   });
   
+
+   
+  const menu = document.querySelector('.js-menu');
+  const nav = document.querySelector('.js-nav');
+  const profile = document.querySelector('.js-profile');
+  const profileImg = document.querySelector('.js-profile-img');
+  const burger = document.querySelector('.js-burger');
+  const logo = document.querySelector('.js-logo');
+  const grid = document.querySelector('.js-grid');
+  const container = document.querySelector('.js-container');
+
+  burger.addEventListener('click', ()=>{  
+  burger.classList.toggle('burger--active');
+  logo.classList.toggle('logo__img-name--active');
+  nav.classList.toggle('nav--active');
+  profile.classList.toggle('profile--active');
+  profileImg.classList.toggle('profile-img--active');
+  menu.classList.toggle('menu--active');
+  grid.classList.toggle('grid--active');
+  container.classList.toggle('container--active');
+  });
